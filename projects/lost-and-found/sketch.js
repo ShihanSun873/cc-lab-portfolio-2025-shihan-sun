14ade3e9f6f23c2570adf68746657ae9c243e7f0
ShihanSun873 <suns404@newschool.edu>
2025-12-12 14:18:48 -0500

    Add initial sketch.js for Lost and Found project

---
+++ projects/lost-and-found/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(600, 400);
+  background(30);
+
+  fill(220);
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  text("Lost and Found", width / 2, height / 2);
+}
+
+function draw() {
+  // 暂时留空
+}
