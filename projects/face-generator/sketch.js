cb08d0bfeaa5db9d8b3cf08069052d37efe8d15a
ShihanSun873 <suns404@newschool.edu>
2025-12-12 14:23:33 -0500

    add face generator sketch

---
+++ projects/face-generator/sketch.js
@@ -0,0 +1,11 @@
+function setup() {
+  createCanvas(600, 400);
+  background(20);
+
+  fill(230);
+  textAlign(CENTER, CENTER);
+  textSize(32);
+  text("Face Generator", width / 2, height / 2);
+}
+
+function draw() {}
