89eab33cbf48bb0205a5f3ad31ed16a9b4b692cf
ShihanSun873 <suns404@newschool.edu>
2025-12-12 14:49:17 -0500

    Implement functional clock display in sketch.js
    Updated the clock display to show hours, minutes, and seconds with appropriate angles and stroke weights.

--- projects/experimental-clock/sketch.js
+++ projects/experimental-clock/sketch.js
@@ -1,10 +1,45 @@
 function setup() {
-  createCanvas(600, 400);
-  background(20);
-
-  fill(230);
-  textAlign(CENTER, CENTER);
-  textSize(32);
-  text("Experimental Clock", width / 2, height / 2);
+  createCanvas(600, 600);
+  angleMode(DEGREES); 
 }
-function draw() {}
+
+function draw() {
+  background(0);
+  translate(width / 2, height / 2);
+  rotate(-90); 
+
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+
+  let scAngle = map(sc, 0, 60, 0, -360);
+  let mnAngle = map(mn + sc/60, 0, 60, 0, -360);
+  let hrAngle = map(hr % 12 + mn/60, 0, 12, 0, -360);
+
+  stroke(1000);
+  noFill();
+  ellipse(0, 0, 400, 400);
+
+  push();
+  rotate(hrAngle);
+  strokeWeight(15);
+  line(0, 0, 100, 0);
+  pop();
+
+  push();
+  rotate(mnAngle);
+  strokeWeight(10);
+  line(0, 0, 140, 0);
+  pop();
+
+  push();
+  rotate(scAngle);
+  strokeWeight(10);
+  stroke(200, 0, 0);
+  line(0, 0, 170, 0);
+  pop();
+
+  stroke(0);
+  fill(0);
+  ellipse(0, 0, 10, 10);
+}
